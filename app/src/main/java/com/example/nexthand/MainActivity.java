e8a0c60acf5b09d3fbe95c285ebd4a9b1c1bdab3
Adar Tulloch <adartulloch@fb.com>
2021-07-12 11:38:44 -0400

    Initial commit, added skeleton fragments

---
+++ app/src/main/java/com/example/nexthand/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.nexthand;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.MenuItem;
+
+import com.example.nexthand.fragments.ComposeFragment;
+import com.example.nexthand.fragments.HomeFragment;
+import com.example.nexthand.fragments.MapFragment;
+import com.example.nexthand.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_map:
+                    selectedFragment = new MapFragment();
+                    break;
+                case R.id.nav_compose:
+                    selectedFragment = new ComposeFragment();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
+}

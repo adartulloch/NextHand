166075070f6a1ebe8919c077c996ec9a33e9f537
Adar Tulloch <adartulloch@fb.com>
2021-07-15 17:59:01 -0400

    Wrote functionality for ItemsAdapter

---
+++ app/src/main/java/com/example/nexthand/feed/ItemsAdapter.java
@@ -0,0 +1,77 @@
+package com.example.nexthand.feed;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.nexthand.R;
+import com.example.nexthand.models.Item;
+import com.example.nexthand.models.User;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
+
+    private Context mContext;
+    private List<Item> items;
+
+    public ItemsAdapter(Context mContext, List<Item> items) {
+        this.mContext = mContext;
+        this.items = items;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ItemsAdapter.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ItemsAdapter.ViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ImageView ivPost;
+        private TextView tvTitle;
+
+        public ViewHolder(@NotNull View itemView) {
+            super(itemView);
+            ivPost = itemView.findViewById(R.id.ivPost);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Item item) {
+            Glide.with(mContext).load(item.getParseFile(Item.KEY_IMAGE).getUrl()).into(ivPost);
+            tvTitle.setText(item.getTitle());
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                //TODO: Implement DetailsFragment, Bundle Item and share with new DetailsFragment
+            }
+        }
+    }
+}

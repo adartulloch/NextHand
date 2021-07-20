2e34db8abe2e981f7651cd39f41a65940983bd67
Adar Tulloch <adartulloch@fb.com>
2021-07-20 15:05:13 -0400

    Cleaned up BottomNavigationView logic

--- app/src/main/java/com/example/nexthand/MainActivity.java
+++ app/src/main/java/com/example/nexthand/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.nexthand;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 import com.example.nexthand.compose.ComposeFragment;
@@ -14,41 +16,37 @@ import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView mBottomNavigationView;
+    private FragmentManager mFragmentManager = getSupportFragmentManager();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        mBottomNavigationView.setSelectedItemId(R.id.nav_home);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
-            Fragment selectedFragment = null;
-
-            switch (item.getItemId()){
-                case R.id.nav_home:
-                    selectedFragment = new com.example.nexthand.feed.HomeFragment();
-                    break;
-                case R.id.nav_map:
-                    selectedFragment = new MapFragment();
-                    break;
-                case R.id.nav_compose:
-                    selectedFragment = new ComposeFragment();
-                    break;
-                case R.id.nav_profile:
-                    selectedFragment = new ProfileFragment();
-                    break;
-            }
-
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-            return true;
-        }
-    };
+            item -> {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_map:
+                        selectedFragment = new MapFragment();
+                        break;
+                    case R.id.nav_compose:
+                        selectedFragment = new ComposeFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                mFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            };
 }

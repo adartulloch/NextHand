61c961043a27be50e73f573a74a5e17dabe643dc
Adar Tulloch <adartulloch@fb.com>
2021-07-23 16:42:13 -0400

    [DB] Updated contact model to reference recipient ParseUser

--- app/src/main/java/com/example/nexthand/models/Contact.java
+++ app/src/main/java/com/example/nexthand/models/Contact.java
@@ -4,9 +4,12 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.io.Serializable;
+
 @ParseClassName("Contact")
-public class Contact extends ParseObject {
+public class Contact extends ParseObject implements Serializable {
     public static final String KEY_USER = "user";
+    public static final String KEY_RECIPIENT = "recipient";
 
     public Contact () {}
 
@@ -14,7 +17,13 @@ public class Contact extends ParseObject {
         return getParseUser(KEY_USER);
     }
 
-    public void setUser (ParseUser user) {
-        put(KEY_USER, user);
+    public void setUser (ParseUser user) { put(KEY_USER, user); }
+
+    public ParseUser getRecipient() {
+        return getParseUser(KEY_RECIPIENT);
+    }
+
+    public void setRecipient(ParseUser recipient) {
+        put(KEY_RECIPIENT, recipient);
     }
 }

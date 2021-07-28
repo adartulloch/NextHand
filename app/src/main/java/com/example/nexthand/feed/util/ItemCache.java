dc7019cc4ab02f90695dec294536a5940189bd23
Adar Tulloch <adartulloch@fb.com>
2021-07-28 15:39:35 -0400

    ItemCache singleton class defined

--- app/src/main/java/com/example/nexthand/feed/util/ItemCache.java
+++ app/src/main/java/com/example/nexthand/feed/util/ItemCache.java
@@ -1,2 +1,24 @@
-package com.example.nexthand.feed.util;public class ItemCache {
+package com.example.nexthand.feed.util;
+
+import android.util.LruCache;
+
+public class ItemCache {
+    public static final int CACHE_MAX_SIZE = 1024;
+    private static ItemCache mInstance;
+    private LruCache<Object, Object> mCache;
+
+    private ItemCache() {
+        mCache = new LruCache<Object, Object>(CACHE_MAX_SIZE);
+    }
+
+    public static ItemCache getInstance() {
+        if (mInstance == null) {
+            mInstance = new ItemCache();
+        }
+        return mInstance;
+    }
+
+    public LruCache<Object, Object> getCache() {
+        return mCache;
+    }
 }
